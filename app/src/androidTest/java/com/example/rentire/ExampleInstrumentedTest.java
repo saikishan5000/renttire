b7d764b96f9494886dcb9ca8e79dea8b1e2ceeb7
Rup-Royofficial <rupayanroy981@gmail.com>
2023-02-08 20:48:50 +0530

    Developed and Created the Register Page

--- app/src/androidTest/java/com/example/rentire/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/rentire/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.renttire;
+package com.example.rentire;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.renttire", appContext.getPackageName());
+        assertEquals("com.example.rentire", appContext.getPackageName());
     }
 }
