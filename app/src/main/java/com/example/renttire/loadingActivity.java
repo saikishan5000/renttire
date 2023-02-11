c932b028bbc29bc21965ccefaccb2962dfd53ca7
Rup-Royofficial <rupayanroy981@gmail.com>
2023-02-11 19:04:52 +0530

    Developed and integrated a loading screen

---
+++ app/src/main/java/com/example/renttire/loadingActivity.java
@@ -0,0 +1,25 @@
+package com.example.renttire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class loadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+//        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(loadingActivity.this,MainActivity.class));
+            }
+        },3000);
+    }
+}
